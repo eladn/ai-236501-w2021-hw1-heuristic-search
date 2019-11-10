f93ab065f1c9b1cb89bd6af871bad8b20085f1eb
Elad <eladnah@gmail.com>
2019-11-10 17:13:18 +0200

    map problem: use new `OperatorResult`, add optional road_cost_fn

--- staff_solution/deliveries/map_problem.py
+++ staff_solution/deliveries/map_problem.py
@@ -1,6 +1,6 @@
 from framework import *
 
-from typing import Iterator, Tuple
+from typing import Iterator, Optional, Callable
 
 
 class MapState(GraphProblemState):
@@ -25,24 +25,28 @@ class MapState(GraphProblemState):
 
 class MapProblem(GraphProblem):
     """
-    Represents a problem on the geographic map.
+    Represents a problem on the streets map.
     The problem is defined by a source location on the map and a destination.
     """
 
     name = 'Map'
 
-    def __init__(self, roads: Roads, source_junction_id: int, target_junction_id: int):
+    def __init__(self, roads: Roads, source_junction_id: int, target_junction_id: int,
+                 road_cost_fn: Optional[Callable[[Link], Cost]] = None,
+                 zero_road_cost: Optional[Cost] = None):
         initial_state = MapState(source_junction_id)
         super(MapProblem, self).__init__(initial_state)
         self.roads = roads
         self.target_junction_id = target_junction_id
-        self.name += '(src: {} dst: {})'.format(source_junction_id, target_junction_id)
+        self.road_cost_fn = road_cost_fn
+        self.zero_road_cost = zero_road_cost
+        self.name += f'(src: {source_junction_id} dst: {target_junction_id})'
     
-    def expand_state_with_costs(self, state_to_expand: GraphProblemState) -> Iterator[Tuple[GraphProblemState, float]]:
+    def expand_state_with_costs(self, state_to_expand: GraphProblemState) -> Iterator[OperatorResult]:
         """
         For a given state, iterates over its successor states.
         The successor states represents the junctions to which there
-        exists a road originates from the given state.
+         exists a road originates from the given state.
         """
 
         # All of the states in this problem are instances of the class `MapState`.
@@ -51,19 +55,37 @@ class MapProblem(GraphProblem):
         # Get the junction (in the map) that is represented by the state to expand.
         junction = self.roads[state_to_expand.junction_id]
 
-        # Iterate over the outgoing roads of the current junction.
-        for link in junction.links:
-            # Create the successor state (it should be an instance of class `MapState`).
-            successor_state = MapState(link.target)
+        # TODO: read the documentation of this method in the base class `GraphProblem.expand_state_with_costs()`.
+        # TODO: finish the implementation of this method.
+        #  Iterate over the outgoing links of the current junction (find the implementation of `Junction`
+        #  type to see the exact field name). For each link:
+        #    (1) Create the successor state (it should be an instance of class `MapState`). This state represents the
+        #        target junction of the current link;
+        #    (2) Calculate the operator cost: if `self.road_cost_fn` is None the operator cost should be
+        #        `link.distance`, otherwise call the function `self.road_cost_fn` with the link as an argument
+        #        and set the operator cost to be the returned value of this call;
+        #    (3) Yield an object of type `OperatorResult` with the successor state and the operator cost (you don't
+        #        have to specify the operator name here). Find the definition of `OperatorResult` and check out its
+        #        field.
 
-            # TODO: calculate the distance between `junction` and the successor's junction.
-            # Use the method `calc_air_distance_from()` of class `Junction` to measure this distance.
-            # Do NOT use `link.distance` here.
-            # operator_cost = 1  # TODO: modify this!
-            operator_cost = junction.calc_air_distance_from(self.roads[link.target])
+        # raise NotImplementedError()  # TODO: remove this line!
+
+        for outgoing_link in junction.outgoing_links:
+            # Create the successor state (it should be an instance of class `MapState`).
+            successor_state = MapState(outgoing_link.target)
+
+            if self.road_cost_fn is None:
+                operator_cost = outgoing_link.distance
+            else:
+                operator_cost = self.road_cost_fn(outgoing_link)
 
             # Yield the successor state and the cost of the operator we used to get this successor.
-            yield successor_state, operator_cost
+            yield OperatorResult(successor_state=successor_state, operator_cost=operator_cost)
+
+    def get_zero_cost(self) -> Cost:
+        if self.zero_road_cost is not None:
+            return self.zero_road_cost
+        return 0.0
 
     def is_goal(self, state: GraphProblemState) -> bool:
         """
@@ -73,5 +95,5 @@ class MapProblem(GraphProblem):
 
         # TODO: modify the returned value to indicate whether `state` is a final state.
         # You may use the problem's input parameters (stored as fields of this object by the constructor).
-        # return state.junction_id == 14593  # TODO: modify this!  #  state.junction_id == self.target_junction_id
+        # return state.junction_id == 14593  # TODO: modify this!
         return state.junction_id == self.target_junction_id
