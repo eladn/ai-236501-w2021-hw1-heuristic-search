00d6104d62f9aa0911fb7847dc4736fb6b3e59de
Elad <eladnah@gmail.com>
2019-11-17 11:30:45 +0200

    solvers: remove the GreedyStochastic & GreedyBestFirst from modules init

--- staff_solution/framework/graph_search/__init__.py
+++ staff_solution/framework/graph_search/__init__.py
@@ -4,8 +4,6 @@ from .uniform_cost import UniformCost
 from .astar import AStar
 from .astar_epsilon import AStarEpsilon
 from .id_astar import IDAStar
-from .greedy_stochastic import GreedyStochastic
-from .greedy_best_first import GreedyBestFirst
 from .anytime_astar import AnytimeAStar
 
-__all__ = ['UniformCost', 'AStar', 'AStarEpsilon', 'IDAStar', 'GreedyStochastic', 'GreedyBestFirst', 'AnytimeAStar'] + graph_problem_interface.__all__
+__all__ = ['UniformCost', 'AStar', 'AStarEpsilon', 'IDAStar', 'AnytimeAStar'] + graph_problem_interface.__all__
