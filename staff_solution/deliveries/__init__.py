1fc9daa68d73e9464518d045b5ae5614dd67a3be
Elad <eladnah@gmail.com>
2019-11-17 08:38:08 +0200

    deliveries init minor change

--- staff_solution/deliveries/__init__.py
+++ staff_solution/deliveries/__init__.py
@@ -4,6 +4,7 @@ from .deliveries_truck_problem_input import *
 from .deliveries_truck_problem import *
 from .deliveries_truck_heuristics import *
 from .cached_map_distance_finder import CachedMapDistanceFinder
+from .cached_air_distance_calculator import CachedAirDistanceCalculator
 
 __all__ = [
     'AirDistHeuristic',
@@ -11,5 +12,6 @@ __all__ = [
     'Delivery', 'DeliveriesTruck', 'DeliveriesTruckState', 'OptimizationObjective',
     'DeliveriesTruckProblemInput', 'DeliveriesTruckProblem',
     'TruckDeliveriesMaxAirDistHeuristic', 'TruckDeliveriesMSTAirDistHeuristic',
-    'TruckDeliveriesSumAirDistHeuristic'
+    'TruckDeliveriesSumAirDistHeuristic',
+    'CachedMapDistanceFinder', 'CachedAirDistanceCalculator'
 ]
