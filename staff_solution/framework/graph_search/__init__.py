81ffde3aa5c532f3bca2d685a0927944d4f1a86b
Elad <eladnah@gmail.com>
2019-11-19 13:06:48 +0200

    remove ID-A*

--- staff_solution/framework/graph_search/__init__.py
+++ staff_solution/framework/graph_search/__init__.py
@@ -1,9 +1,7 @@
-
 from .graph_problem_interface import *
 from .uniform_cost import UniformCost
 from .astar import AStar
 from .astar_epsilon import AStarEpsilon
-from .id_astar import IDAStar
 from .anytime_astar import AnytimeAStar
 
-__all__ = ['UniformCost', 'AStar', 'AStarEpsilon', 'IDAStar', 'AnytimeAStar'] + graph_problem_interface.__all__
+__all__ = ['UniformCost', 'AStar', 'AStarEpsilon', 'AnytimeAStar'] + graph_problem_interface.__all__
