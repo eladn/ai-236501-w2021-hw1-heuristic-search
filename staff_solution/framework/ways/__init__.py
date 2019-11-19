a402e37b0cf52403dac3c8658feb9dc12b5f32f3
Elad <eladnah@gmail.com>
2019-11-19 13:31:36 +0200

    rename graph ==> streets_map

--- staff_solution/framework/ways/__init__.py
+++ staff_solution/framework/ways/__init__.py
@@ -3,7 +3,7 @@ lists the names you will need and what you can import with:
 >>> from framework.ways import
 """
 
-from .graph import Junction, StreetsMap, Link, ROAD_SPEEDS, MIN_ROAD_SPEED, MAX_ROAD_SPEED, kmph_to_mpm
+from .streets_map import Junction, StreetsMap, Link, ROAD_SPEEDS, MIN_ROAD_SPEED, MAX_ROAD_SPEED, kmph_to_mpm
 
 __all__ = ['Junction', 'Link', 'StreetsMap',
            'ROAD_SPEEDS', 'MIN_ROAD_SPEED', 'MAX_ROAD_SPEED', 'kmph_to_mpm']
